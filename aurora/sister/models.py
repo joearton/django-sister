cd4215002a4d2f08e9e8e42712cdcd80caa33545
joearton <joearton.id@gmail.com>
2022-03-15 15:10:48 +0700

    Adding download features

--- aurora/sister/models.py
+++ aurora/sister/models.py
@@ -7,6 +7,33 @@ from django.utils.text import slugify
 import os
 
 
+class University(BaseModel):
+    id_pt = models.UUIDField(_('ID PT'), editable=False, unique=True)
+    kode = models.CharField(_('Kode'), max_length=16)
+    nama = models.CharField(_('Nama'), max_length=128)
+    telepon = models.CharField(_('Telepon'), max_length=128, null=True, blank=True)
+    faximile = models.CharField(_('Faximile'), max_length=128, null=True, blank=True)
+    email = models.CharField(_('E-mail'), max_length=65, null=True, blank=True)
+    website = models.CharField(_('Website'), max_length=64, null=True, blank=True)
+    jalan = models.CharField(_('Jalan'), max_length=128, null=True, blank=True)
+    dusun = models.CharField(_('Dusun'), max_length=128, null=True, blank=True)
+    rt = models.IntegerField(_('RT'), null=True, blank=True)
+    rw = models.IntegerField(_('RW'), null=True, blank=True)
+    kelurahan = models.CharField(_('Kelurahan'), max_length=128, null=True, blank=True)
+    kode_pos = models.CharField(_('Kode Pos'), max_length=16, null=True, blank=True)
+    nama_wilayah = models.CharField(_('Nama Wilayah'), max_length=128, null=True, blank=True)
+    sk_pendirian = models.CharField(_('SK Pendirian'), max_length=128, null=True, blank=True)
+    tanggal_sk = models.CharField(_('Tanggal SK Pendirian'), max_length=128, null=True, blank=True)
+    status = models.CharField(_('Status'), max_length=128, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('University')
+        verbose_name_plural = _('Universities')
+
+    def __str__(self):
+        return self.nama
+    
+
 class Unit(BaseModel):
     unit_id = models.UUIDField(_('Unit'), editable=False)
     nama = models.CharField(_('Nama'), max_length=128)
@@ -39,7 +66,6 @@ class SDM(BaseModel):
     unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True, blank=True, related_name='people')
     metadata = models.JSONField(_('Metadata'), null=True, blank=True, default=dict)
     status = models.IntegerField(_('Status'), default=1, choices=CHOICES_STATUS)
-
 
     class Meta:
         verbose_name = _('SDM')
